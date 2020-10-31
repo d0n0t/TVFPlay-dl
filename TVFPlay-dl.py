fefea5daa0095947acae366ba0635ad1bc7048d4
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-10-31 17:49:10 +0530

    Update TVFPlay-dl.py

--- TVFPlay-dl.py
+++ TVFPlay-dl.py
@@ -31,17 +31,17 @@ except:
 
 brightcove_url = "https://edge.api.brightcove.com/playback/v1/accounts/%s/videos/%s" % (account_id, video_id)
 
-headers = {
-        "Accept" : "application/json;pk=BCpkADawqM26dWgHi4x9Lu_uTkYQJxCTuCaHYLBE4aZP8Rt0mwH90-U1yNE95i08SZaACJN3ZIAsG0Jy8QkazZ8rgTc1\
-        ySWLc8VG55XO42u92I2xwV1ObWXczNFBDZv8fXjZ9cIXZqKwocg_1dwa07eFUx3VQyXBEP5hz-WZt9pvog4edZMnUPKnuo2yJ_ZNWLRAMWjV_lTxIuSb0UoxWf9v\
-        sfvOvcDVHWAucb5zFpUSLq8wZ7_HWtRj5_tgBVW8vG00k81rxc39Tu-WwB_q2bk6IDYQRh1ZNqpce2dvKl68lpL6mm080lz5zlmhlV7uWNOJWRJvJAFtr2TgUpEq\
-        WHEsZah3vbafrc1mZcTlu_4KyzMgnHfZCHsP5ATD9saSiOCTjbzyB3ISMw6m7yGdPGQzRf5Y3bnmeWNKUi_kPGyBoOa6-ik3msfGBeey_PgI7X3YD19DvocW3sk3\
-        188weIK5cxcebTpyMycQik6fPkt0jRIfXELfekgxz-WIqKCvGMGs0EnNLJRMSTE46aWSqGCRoAKXBRHNMktKBzM6SfGlTC2inesdzwXIqpxG8NqbnBUxSplboiOJ\
-        bz-4MjS16YM_8Pj_UZFs3uFYtSgAyVvNjBnn9Vxv0GRf32U",
-        "Origin": "https://tvfplay.com",
-        "Referer" : url,
-        "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
-        }
+headers = headers = {
+    "User-Agent":"Mozilla/5.0 (Linux; Android 10; SM-J400F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36",
+    "Referer":"https://www.zee5.com",
+    "Accept":"*/*",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Accept-Language":"en-US,en;q=0.9",
+    "Origin":"https://www.zee5.com",
+    "sec-fetch-dest":"empty",
+    "sec-fetch-mode":"cors",
+    "sec-fetch-site":"same-site",
+}
 
 brightcove_response = requests.get(brightcove_url, headers=headers)
 
